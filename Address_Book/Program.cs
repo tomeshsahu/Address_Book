523045318b6815dcd281b2f3d4e401fe6f7757e1
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-20 22:51:38 +0530

    [TOMESHWAR]ADD:-ADDRESS BOOK UPDATE SYSTEM

---
+++ Address_Book/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Address_Book;
+
+class program
+{
+    
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("WelCome To Address Book");
+        Contact aBook = new Contact();
+
+        AddressBook book = new AddressBook();
+
+
+
+        while (true)
+        {
+            Console.WriteLine("Select Number\n1.Add Contact\n2.Display Contact\n3.Update Contact");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    book.addContact();
+                    break;
+                case 2:
+                    book.Display();
+                    break;
+                case 3:
+                    book.Update();
+                    break;
+            }
+        }
+
+    }
+
+}
