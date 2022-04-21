fd8873bcf7d701399c94062ce132bb6c167d7411
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-21 08:02:53 +0530

    [TOMESHWAR]ADD:-DISPLAY THE CONTACT SYSTEM

--- Address_Book/AddressBook.cs
+++ Address_Book/AddressBook.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace Address_Book
 {
     public class AddressBook
     {
-        public string WelcomeMessage()
+        List<Contact> addressBook = new List<Contact>();
+        public AddressBook()
         {
-            string mesaage = "Welcome In This Project";
-            return mesaage;
+            Contact address1 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt32(Console.ReadLine()),
+                PhoneNumber = Convert.ToInt64(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            Contact address2 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt32(Console.ReadLine()),
+                PhoneNumber = Convert.ToInt64(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.Add(address1);
+            addressBook.Add(address2);
         }
+        public void AddContactToAddressBook(Contact contact)
+        {
+            addressBook.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressBook)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address + " " + contact.City + " " + contact.State + " " + " " + contact.Zip + " " + contact.PhoneNumber+ " " + contact.Email);
+            }
+        }
+
     }
 }
--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -5,30 +5,21 @@ class program
     public static void Main(String[] args)
     {
         Console.WriteLine("Enter the Address Details");
-        Contact contact = new Contact();
-        Console.WriteLine("Enter FirstName");
-        contact.FirstName = Convert.ToString(Console.ReadLine());
 
-        Console.WriteLine("Enter LastName");
-        contact.LastName = Convert.ToString(Console.ReadLine());
+        AddressBook addressBook = new AddressBook();
+        addressBook.Display();
 
-        Console.WriteLine("Enter Address");
-        contact.Address = Convert.ToString(Console.ReadLine());
-
-        Console.WriteLine("Enter City");
-        contact.City = Convert.ToString(Console.ReadLine());
-
-        Console.WriteLine("Enter State");
-        contact.State = Convert.ToString(Console.ReadLine());
-
-        Console.WriteLine("Enter Zip");
-        contact.Zip = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter Phone Number");
-        contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-
-        Console.WriteLine("Enter Email");
-        contact.Email = Convert.ToString(Console.ReadLine());
+    Contact address=new Contact ();
+        address.FirstName =Console.ReadLine();
+        address.LastName =Console.ReadLine();
+        address.Address=Console.ReadLine();
+        address.City = Console.ReadLine();
+        address.State = Console.ReadLine();
+        address.Zip =Convert.ToInt32(Console.ReadLine());
+        address.PhoneNumber =Convert.ToInt64 (Console.ReadLine());
+        address.Email = Console.ReadLine();
+        Console.WriteLine(address.FirstName+" "+ address.LastName+" "+ address.Address+" "+ address.City+" "+ address.State+" "+ address.Zip+" "+ address.PhoneNumber+" " + address.Email);
+        
 
     }
 }
